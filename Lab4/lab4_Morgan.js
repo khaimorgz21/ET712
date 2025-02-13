10ff6096d797c6c7926b32d5dc72306fdba46e00
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-02-13 15:38:13 -0500

    Update lab4_Morgan.js

--- Lab4/lab4_Morgan.js
+++ Lab4/lab4_Morgan.js
@@ -65,3 +65,43 @@ function checktemp(temperature){
 let t = 100
 let temp_result = checktemp(t)
 console.log(`Is the temperature greater than 75? ${temp_result}`)
+
+console.log("Makhai Morgan")
+console.log("\n------ example 7: built-in function -----------")
+// built-in function in JS is called as a global variable 
+// Math function
+let num_pi = Math.PI
+console.log(`JS PI value = ${num_pi}`)
+// round() methods returns the nearest integer
+console.log(`rounded off PI = ${Math.round(num_pi)}`)
+// ceil() method returns the round up of a number
+console.log(`ceil method applied to PI = ${Math.ceil(num_pi)}`)
+// pow() method returens the value of baser to the pwoer of exponent
+console.log(`2^3 ${Math.pow(2,3)}`)
+// square root method --> sqrt()
+console.log(`square root of 8 = ${Math.sqrt(8)}`)
+// random number generator. random() method returns a random number between 0 and 1
+console.log(`Random number = ${Math.random()}`)
+// random number between 1 and 9
+let n = 1 + (Math.random()*8)
+console.log(`Number between 1 and 9 = ${n}`)
+console.log("\n------ example 8: random number -----------")
+// function to randomly generate a number between -5 and 5
+function randnumber() {
+    let p = -5 + Math.round(Math.random()*10)
+    return p;
+}
+
+console.log("\n------ example 9: random number in a list (array) -----------")
+// function to randomly pick a color from a list of colors
+let colors = ["red", "green", "blue", "magenta", "gray"]
+function randomcolor(){
+    let list_size = colors.length  // find the lenght of the list
+    let last_index = list_size - 1 // find the last index of the list
+    let random_index = Math.random(Math.random()*last_index) // randomly pick a number between 0 and the last index
+    return colors[random_index]       // return the random picked color
+}
+
+console.log(`Color picked = ${randomcolor()}`)
+
+console.log("\n------ EXERCISE -----------")
