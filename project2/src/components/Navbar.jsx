5d4f1000874f4fca27981993e9421095978d78ca
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-05-27 23:02:28 -0400

    Update Navbar.jsx

--- project2/src/components/Navbar.jsx
+++ project2/src/components/Navbar.jsx
@@ -1,1 +1,15 @@
-
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { CartContext } from '../context/CartContext';
+export default function Navbar() {
+  const { cartItems } = useContext(CartContext);
+  const totalQty = cartItems.reduce((s,i)=>s+i.quantity,0);
+  return (
+    <nav style={{background: 'var(--color-primary)', padding: '1rem', color: '#fff'}}>
+      <div className="container" style={{display:'flex', justifyContent:'space-between'}}>
+        <div><Link to="/category1" style={{color:'#fff', marginRight:'1rem'}}>Cat1</Link><Link to="/category2" style={{color:'#fff', marginRight:'1rem'}}>Cat2</Link><Link to="/category3" style={{color:'#fff'}}>Cat3</Link></div>
+        <Link to="/cart" style={{color:'#fff'}}>Cart ({totalQty})</Link>
+      </div>
+    </nav>
+  );
+}
