743c27f4fb953912bcf6c9cb566fb36d57183bf3
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-05-27 22:32:00 -0400

    Update App.jsx

--- project2/src/App.jsx
+++ project2/src/App.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import ProductPage from './components/ProductPage';
+import CartPage from './components/CartPage';
+import { CartProvider } from './context/CartContext';
 
+export default function App() {
+  return (
+    <CartProvider>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Navigate to="/category1" replace />} />
+            <Route path="/category:cat" element={<ProductPage />} />
+            <Route path="/cart" element={<CartPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
