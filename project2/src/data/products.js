a2fffa092cd0767d124d1770ec736d1b906b5cc1
khaimorgz21 <158099433+khaimorgz21@users.noreply.github.com>
2025-05-28 00:54:35 -0400

    Update products.js

--- project2/src/data/products.js
+++ project2/src/data/products.js
@@ -1,14 +1,69 @@
 export const productsByCategory = {
-  category1: Array.from({length:9},(_,i)=>(
-    { id: i+1, title: `Category1 - Product ${i+1}`,
-      image: `/images/cat1-${i+1}.jpg`,
-      description: `Description for product ${i+1} in category1.`
+  category1: [ 
+    { id: 1, 
+     title: 'Vintage MJ sneakers AJ1',
+      image: `/images/AirJordan1.jpg`,
+      description: `Attractive'`
+      Price: 355.00
+    },
+  category1: [ 
+    { id: 2, 
+     title: 'Vintage MJ sneakers AJ2',
+      image: `/images/AirJordan2.jpg`,
+      description: `Very few available'`
+      Price: 455.00
+    },
+  category1: [ 
+    { id: 3, 
+     title: 'Vintage MJ sneakers AJ3',
+      image: `/images/AirJordan3.jpg`,
+      description: `has the best designs'`
+      Price: 555.00
+    },
+
+  category1: [ 
+    { id: 4, 
+     title: 'Vintage MJ sneakers AJ4',
+      image: `/images/AirJordan4.jpg`,
+      description: `Fits really well'`
+      Price: 655.00
+    }, 
+  category1: [ 
+    { id: 5, 
+     title: 'Vintage MJ sneakers AJ5',
+      image: `/images/AirJordan5.jpg`,
+      description: `People's choice'`
+      Price: 755.00
+    },
+  category1: [ 
+    { id: 6, 
+     title: 'Vintage MJ sneakers AJ6',
+      image: `/images/AirJordan6.jpg`,
+      description: `always in demand'`
+      Price: 855.00
+    },
+  category1: [ 
+    { id: 7, 
+     title: 'Vintage MJ sneakers AJ7',
+      image: `/images/AirJordan7.jpg`,
+      description: `for all occasions'`
+      Price: 955.00
+    },
+   category1: [ 
+    { id: 8, 
+     title: 'Vintage MJ sneakers AJ8',
+      image: `/images/AirJordan8.jpg`,
+      description: `highly rated'`
+      Price: 1055.00
     }
-  )),
-  category2: Array.from({length:9},(_,i)=>(
-    { id: i+101, title: `Category2 - Product ${i+1}`, image: `/images/cat2-${i+1}.jpg`, description: `Description for product ${i+1} in category2.` }
-  )),
-  category3: Array.from({length:9},(_,i)=>(
-    { id: i+201, title: `Category3 - Product ${i+1}`, image: `/images/cat3-${i+1}.jpg`, description: `Description for product ${i+1} in category3.` }
+   category1: [ 
+    { id: 9, 
+     title: 'Vintage MJ sneakers AJ9',
+      image: `/images/AirJordan9.jpg`,
+      description: `The best'`
+      Price: 1155.00
+    },
+]     
   )),
 };
+  
